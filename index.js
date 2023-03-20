e43cb6644c99886631b0d8276fa938a5d1a655f9
skashyap061998 <skashyap061998@gmail.com>
2023-03-20 12:17:19 +0530

    changes in index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express()
 const connection = require("./db")
 const jwt = require("jsonwebtoken")
 const UserModel = require('./models/auth.model')
-const postRoute = require('./routes/postROute')
+const postRoute = require('./routes/postRoute')
 const commentRoute = require('./routes/comment')
 const cors = require('cors')
 
