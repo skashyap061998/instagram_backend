40b16711db3c67044230a3f3c29fc20492675777
skashyap061998 <skashyap061998@gmail.com>
2023-03-20 11:42:55 +0530

    backend deployment initiated

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const auth = require('./routes/auth')
 require('dotenv').config()
-const port = process.env.PORT 
+const port = process.env.PORT || 8080
 const app = express()
 const connection = require("./db")
 const jwt = require("jsonwebtoken")
